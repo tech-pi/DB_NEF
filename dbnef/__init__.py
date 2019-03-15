81f20e616c4e9bec0bd4ac0b8cf6ae5ea866ceb0
Minghao <mh.guo0111@gmail.com>
2019-03-15 20:29:04 +0800

    initial

---
+++ dbnef/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+'''
+@author: Minghao Guo
+@contact: mh.guo0111@gmail.com
+@software: srf_ct
+@file: __init__.py
+@date: 3/15/2019
+@desc:
+'''
---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+MAJOR = 0
+MINOR = 6
+MICRO = 1  # 0 for alpha, 1 for beta, 2 for release candicate, 3 for release
+MINOR_SUB = 11
+
+
+def get_version(major, minor, micro, minor_sub):
+    from time import time
+    from datetime import datetime
+    timestamp = int(time())
+    short_version = str(major) + '.' + str(minor) + '.' + str(micro) + '.' + str(minor_sub)
+    full_version = short_version + '.' + str(timestamp)
+    time_string = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+    return short_version, full_version, time_string
+
+
+SHORT_VERSION, FULL_VERSION, TIME_STRING = get_version(MAJOR, MINOR, MICRO, MINOR_SUB)
+
+
+def write_version_py(filename = 'srfnef/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM SRFNEF SETUP.PY
+# 
+short_version = '%(short_version)s'
+full_version = '%(full_version)s'
+generated_time = '%(time_string)s'
+    
+    """
+    with open(filename, 'w') as fin:
+        fin.write(cnt % {'short_version': SHORT_VERSION,
+                         'full_version': FULL_VERSION,
+                         'time_string': TIME_STRING})
+
+
+write_version_py()
+
+setup(name = 'dbnef',
+      version = FULL_VERSION,
+      py_modules = ['dbnef'],
+      description = 'DATABASE -- Not Enough Functions',
+      author = 'Minghao Guo',
+      author_email = 'mh.guo0111@gmail.com',
+      license = 'Apache',
+      # packages = ['srfnef'],
+      packages = find_packages(),
+      install_requires = [
+
+      ],
+      zip_safe = False,
+      entry_points = '''
+
+      ''',
+      )
