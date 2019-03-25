8284ed1d637b3d77a86fd35fa69783bc54c04efe
Minghao <mh.guo0111@gmail.com>
2019-03-25 23:13:29 +0800

    temporally finished

--- dbnef/config.py
+++ dbnef/config.py
@@ -8,17 +8,25 @@
 @desc:
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+engine = create_engine('postgresql://postgres:postgres@localhost/test_database3', echo = True)
 
-engine = create_engine('postgresql://postgres:postgres@localhost/test_database', echo = True)
+#
+# def create_session():
+#     Session = sessionmaker(bind = engine)
+#     return Session()
+#
+#
+# def create_db_session():
+#     db_session = scoped_session(sessionmaker(autocommit = False,
+#                                              autoflush = False,
+#                                              bind = engine))
+#     return db_session
 
-
-def create_session():
-    from sqlalchemy.orm import sessionmaker
-    Session = sessionmaker(bind = engine)
-    return Session()
-
+Session = sessionmaker(bind = engine)
+session = Session()
 
 Base = declarative_base()
 Base.metadata.bind = engine
--- dbnef/create_table_class.py
+++ dbnef/create_table_class.py
@@ -1,67 +1,82 @@
-# encoding: utf-8
-'''
-@author: Minghao Guo
-@contact: mh.guo0111@gmail.com
-@software: srf_ct
-@file: add_and_update_object.py
-@date: 3/12/2019
-@desc:
-'''
-
-from typing import List
-
-from sqlalchemy import Column, Integer, Float, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, Boolean
 from sqlalchemy.dialects import postgresql
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-import numpy as np
+import typing
 
-from .typing import TYPE_BIND, is_dataclass
 from .utils import convert_Camal_to_snake
-from .config import Base, engine
+from .utils import TABLE_TYPE_BIND, is_dataclass
+from .config import Base
 
 
-def create_table_class(cls: type):
+def create_table_class(cls: type, *, commit = False):
+    try:
+        cls.fields()
+    except:
+        raise ValueError(f'`fields()` is not implemented in class `{cls.__name__}`')
+
+    table_name = convert_Camal_to_snake(cls.__name__)
     table_class_name = cls.__name__ + 'Table'
-    if 'TYPE_BIND' in globals() and table_class_name in TYPE_BIND.keys():
-        table_class = TYPE_BIND[table_class_name]
+    if table_class_name in TABLE_TYPE_BIND.keys():
+        table_cls = TABLE_TYPE_BIND[table_class_name]
     else:
-        table_name = convert_Camal_to_snake(cls.__name__)
-        kwargs = {'__tablename__': table_name, 'id': Column(Integer, primary_key = True)}
-
-        for key, spec in cls.fields().items():
-            if spec.type is int:
-                kwargs.update({key: Column(Integer)})
-            elif spec.type is float:
-                kwargs.update({key: Column(Float)})
-            elif spec.type is bool:
-                kwargs.update({key: Column(Boolean)})
-            elif spec.type is str:
-                kwargs.update({key: Column(String)})
-            elif spec.type is List[int]:
-                kwargs.update({key: Column(postgresql.ARRAY(Integer, dimensions = 1))})
-            elif spec.type is List[float]:
-                kwargs.update({key: Column(postgresql.ARRAY(Float, dimensions = 1))})
-            elif spec.type is List[bool]:
-                kwargs.update({key: Column(postgresql.ARRAY(Boolean, dimensions = 1))})
-            elif spec.type is List[str]:
-                kwargs.update({key: Column(postgresql.ARRAY(String, dimensions = 1))})
-            elif spec.type is np.ndarray:
-                kwargs.update({key: Column(String)})
-            elif is_dataclass(spec.type):
-                kwargs.update({key + '_id': Column(Integer, ForeignKey(key + '.id'))})
-                kwargs.update({key: relationship(spec.type.__name__ + 'Table')})
+        kwargs = {'__tablename__': table_name,
+                  'id': Column(Integer, primary_key = True)}
+        kwargs.update(({'datetime': Column(String)}))
+        kwargs.update(({'creator': Column(String)}))
+        kwargs.update(({'status': Column(String)}))
+        kwargs.update({'labels': Column(postgresql.ARRAY(String, dimensions = 1))})
+        for key, val in cls.fields().items():
+            if key == 'data':
+                kwargs.update({'data': Column(String, nullable = True)})
             else:
-                raise NotImplementedError(f'type {spec.type} is not implemented yet.')
-
-        kwargs.update({'__hash__': Column(String)})
-        table_class = type(table_class_name, (Base,), kwargs)
-        print(kwargs)
-    TYPE_BIND.update({table_class_name: table_class})
-    return table_class
+                if val.type is int:
+                    kwargs.update({key: Column(Integer, nullable = True)})
+                elif val.type is float:
+                    kwargs.update({key: Column(Float, nullable = True)})
+                elif val.type is bool:
+                    kwargs.update({key: Column(Boolean, nullable = True)})
+                elif val.type is str:
+                    kwargs.update({key: Column(String, nullable = True)})
+                elif val.type is typing.List[float]:
+                    kwargs.update(
+                        {key: Column(postgresql.ARRAY(Float, dimensions = 1), nullable = True)})
+                elif val.type is typing.List[int]:
+                    kwargs.update(
+                        {key: Column(postgresql.ARRAY(Integer, dimensions = 1), nullable = True)})
+                elif val.type is typing.List[str]:
+                    kwargs.update(
+                        {key: Column(postgresql.ARRAY(String, dimensions = 1), nullable = True)})
+                elif val.type is typing.List[bool]:
+                    kwargs.update(
+                        {key: Column(postgresql.ARRAY(Boolean, dimensions = 1), nullable = True)})
+                elif val.type is list:
+                    kwargs.update(
+                        {key: Column(postgresql.ARRAY(Float, dimensions = 1), nullable = True)})
+                elif val.type.__name__ + 'Table' in TABLE_TYPE_BIND.keys():
+                    sub_table_class = TABLE_TYPE_BIND[val.type.__name__ + 'Table']
+                    sub_table_name = sub_table_class.__tablename__
+                    kwargs.update(
+                        {sub_table_name + '_id': Column(Integer, ForeignKey(sub_table_name + '.id'),
+                                             nullable = True)})
+                    kwargs.update({key: relationship(val.type.__name__ + 'Table')})
+                elif is_dataclass(val.type):
+                    sub_table_class = create_table_class(val.type, commit = commit)
+                    sub_table_name = sub_table_class.__tablename__
+                    kwargs.update({sub_table_name + '_id': Column(Integer, ForeignKey(
+                        sub_table_name + '.id'))})
+                    kwargs.update({key: relationship(val.type.__name__ + 'Table')})
+                else:
+                    raise NotImplementedError(f'type {val.type} is not implemented yet.')
+        kwargs.update({'__hash__': Column(String, nullable = True)})
+        table_cls = type(table_class_name, (Base,), kwargs)
+        TABLE_TYPE_BIND.update({table_class_name: table_cls})
+    return table_cls
 
 
 def create_table(cls: type, *, commit = False):
-    create_table_class(cls)
+    table_cls = create_table_class(cls, commit = commit)
+
     if commit:
-        Base.metadata.bind = engine
         Base.metadata.create_all()
+    return table_cls
