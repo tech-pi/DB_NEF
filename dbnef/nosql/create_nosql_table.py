bb47077c7a3bf0c2c0cd534509c68f8040fc9f3b
Minghao <mh.guo0111@gmail.com>
2019-04-11 17:11:14 +0800

    when create table, auto make resource and schema folders.

--- dbnef/nosql/create_nosql_table.py
+++ dbnef/nosql/create_nosql_table.py
@@ -7,9 +7,19 @@
 @date: 4/9/2019
 @desc:
 '''
+import os
+
 from sqlalchemy import Column, Integer, String
 
 from dbnef.config import Base
+from dbnef.utils import DATABASE_PATH, RESOURCE_PATH, SCHEMA_PATH
+
+if not os.path.isdir(DATABASE_PATH):
+    os.mkdir(DATABASE_PATH)
+if not os.path.isdir(RESOURCE_PATH):
+    os.mkdir(RESOURCE_PATH)
+if not os.path.isdir(SCHEMA_PATH):
+    os.mkdir(SCHEMA_PATH)
 
 
 class NosqlTable(Base):
