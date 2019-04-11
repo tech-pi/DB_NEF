40e62ff354eacef580b7d577df7310d02d158059
Minghao <mh.guo0111@gmail.com>
2019-04-11 17:12:26 +0800

    1 fix query issues. 2 can recognize None now

--- dbnef/nosql/query.py
+++ dbnef/nosql/query.py
@@ -26,9 +26,12 @@ def query_fields_with_hash(hashes: list = None, *, fields = None):
         dct = {}
         for key, val in session.query(NosqlTable.key, NosqlTable.val).filter(NosqlTable.hash == \
                                                                              hsh).all():
-            if val.startswith('sha256'):
+            if val is None:
+                val = None
+            elif val.startswith('sha256'):
                 val = query_fields_with_hash(val, fields = fields)['0']
-            if key in fields or fields is None:
+
+            if fields is None or key in fields:
                 if key in dct:
                     if not isinstance(dct[key], list):
                         dct[key] = [dct[key], val]
@@ -45,44 +48,25 @@ def query_with_hash(hashes: list = None):
     return query_fields_with_hash(hashes)
 
 
-#
-# def query_with_hash(hashes: list = None):
-#     if hashes is None:
-#         return {}
-#     if not isinstance(hashes, list):
-#         hashes = [hashes]
-#
-#     Session = sessionmaker(bind = engine)
-#     session = Session()
-#     out = {}
-#     for ind, hsh in enumerate(hashes):
-#         dct = {}
-#         for key, val in session.query(NosqlTable.key, NosqlTable.val).filter(NosqlTable.hash == \
-#                                                                              hsh).all():
-#             if val.startswith('sha256'):
-#                 val = query_with_hash(val)['0']
-#             if key in dct:
-#                 if not isinstance(dct[key], list):
-#                     dct[key] = [dct[key], val]
-#                 else:
-#                     dct[key].append(val)
-#             else:
-#                 dct[key] = val
-#         out.update({str(ind): dct})
-#     return out
-
 def search(filters: dict = None):
     if filters is None:
         return {}
     Session = sessionmaker(bind = engine)
     session = Session()
-    cond = True
+    cond = []
     for key, val in filters.items():
-        if isinstance(val, list):
-            cond = cond and getattr(NosqlTable, 'key') == key and getattr(NosqlTable, 'val') in val
+        if not isinstance(val, list):
+            val = [val]
+        if None not in val:
+            cond.extend(
+                [getattr(NosqlTable, 'key') == key, getattr(NosqlTable, 'val').in_(val)])
         else:
-            cond = cond and getattr(NosqlTable, 'key') == key and getattr(NosqlTable, 'val') == val
-    return list({out[0] for out in session.query(NosqlTable.hash).filter(cond).all()})
+            from sqlalchemy import or_
+            cond.extend(
+                [getattr(NosqlTable, 'key') == key, or_(getattr(NosqlTable, 'val').in_(val),
+                                                        getattr(NosqlTable, 'val').is_(None))])
+
+    return list({out[0] for out in session.query(NosqlTable.hash).filter(*cond).all()})
 
 
 def query_fields(filters: dict = None, *, fields = None):
